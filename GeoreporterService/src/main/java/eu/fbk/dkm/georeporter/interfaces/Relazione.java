762f28ec2b14fd623ae25af25d8ada11ee7790f1
gaetanocalabrese <calabrese@fbk.eu>
2017-10-31 16:11:03 +0100

    interfacce

---
+++ GeoreporterService/src/main/java/eu/fbk/dkm/georeporter/interfaces/Relazione.java
@@ -0,0 +1,28 @@
+package eu.fbk.dkm.georeporter.interfaces;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+//@XmlType(propOrder = { "id", "nome", "cognome", "eta", "telefono", "email"  })
+public class Relazione {
+    String uriDomain;
+    String nomerelazione;
+    String uriRange;
+	public String getUriDomain() {
+		return uriDomain;
+	}
+	public void setUriDomain(String uriDomain) {
+		this.uriDomain = uriDomain;
+	}
+	public String getNomerelazione() {
+		return nomerelazione;
+	}
+	public void setNomerelazione(String nomerelazione) {
+		this.nomerelazione = nomerelazione;
+	}
+	public String getUriRange() {
+		return uriRange;
+	}
+	public void setUriRange(String uriRange) {
+		this.uriRange = uriRange;
+	}
+}
