97dab19a2134464833bb6e42a1dc48c286087d90
gaetanocalabrese <calabrese@fbk.eu>
2019-02-15 16:16:04 +0100

    caricamento volano e andrano

--- GeoreporterWrappers/src/eu/fbk/dkm/georeporter/tn/wrappers/mappinginsert/ImportGeoreporter.java
+++ GeoreporterWrappers/src/eu/fbk/dkm/georeporter/tn/wrappers/mappinginsert/ImportGeoreporter.java
@@ -7,6 +7,12 @@ import java.util.Date;
 import java.util.ResourceBundle;
 
 import eu.fbk.dkm.georeporter.config.ConfigurationManager;
+import eu.fbk.dkm.georeporter.ita.wrappers.mappinginsert.MappingInsertFabIdeITA;
+import eu.fbk.dkm.georeporter.ita.wrappers.mappinginsert.MappingInsertFabIndirizzoITA;
+import eu.fbk.dkm.georeporter.ita.wrappers.mappinginsert.MappingInsertFabUiNoteITA;
+import eu.fbk.dkm.georeporter.ita.wrappers.mappinginsert.MappingInsertParFonITA;
+import eu.fbk.dkm.georeporter.ita.wrappers.mappinginsert.MappingInsertSogITA;
+import eu.fbk.dkm.georeporter.ita.wrappers.mappinginsert.MappingInsertTitITA;
 
 
 
@@ -25,7 +31,7 @@ public class ImportGeoreporter {
 
 	public  void importaUnitaImmobiliariITA2(String filePath,String fileHeader, String fileMappings) {
 		
-		MappingInsertFabIde_ITA fabIDE=new MappingInsertFabIde_ITA(targetURL);
+		MappingInsertFabIdeITA fabIDE=new MappingInsertFabIdeITA(targetURL);
 		fabIDE.run(filePath,fileHeader,fileMappings);
 		
 		
@@ -166,7 +172,13 @@ public  void importaTributiUtenzaICI_IMU_NudaProprieta(String filePath, String f
 	MappingInsertTUIciImuNP insicinp = new MappingInsertTUIciImuNP(targetURL); 
 	insicinp.run(filePath, fileMappings);
 }  
+
+public  void importaParticelleFondiarieITA(String filePath,String fileHeader, String fileMappings) {
+	MappingInsertParFonITA inppartfon=new MappingInsertParFonITA(targetURL); 
+	inppartfon.run(filePath, fileHeader, fileMappings);
+}    
   
+
 public  void importaParticelleFondiarie(String filePath,String fileHeader, String fileMappings,String codiceComuneCatastale) {
 	MappingInsertParFon inppartfon=new MappingInsertParFon(targetURL); 
 	inppartfon.run(filePath, fileHeader, fileMappings,codiceComuneCatastale);
@@ -193,6 +205,9 @@ public  void importaTitolaritaParticelleFondiarie(String filePath,String fileHea
 
 
 
+/**
+ * @param args
+ */
 public static void main(String[] args) {
 	
 	
@@ -206,24 +221,177 @@ public static void main(String[] args) {
 		System.out.println(dateFormat.format(date)); //2016/11/16 12:08:43
         String serviceURL="http://localhost:8081/GeoreporterService/servizio/rest/";
 		ImportGeoreporter importgr= new ImportGeoreporter(serviceURL);
-	 
-		
-		
-		// importgr.importaUnitaImmobiliariITA2("file/ITA_file/A281305833_1.FAB","file/ITA_headerfilefab.csv","file/file_mapping/mappingFabbricatiUI_ITA.json");
-        // importgr.importaSoggettiFabbricatiITA("file/ITA_file/A281305833_1.SOG","file/ITA_File/headerfilesogITA.csv","file/file_mapping/mappingSoggettiFabbricati_ITA.json");
-		/// importgr.importaTitolaritaFabbricatiITA("file/ITA_file/A281305833_1.TIT","file/TN_header/headerfiletitITA.csv","file/file_mapping/mappingTitolaritaFabITA.json"); 
-				
 
 		
-//********************* FABBRICATI *************************************//		
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	/////////////////////////// ANDRANO ////////////////////////////////////////////////////////	
+		
+		importgr.importaUnitaImmobiliariITA1("file/Andrano/UnitaImmobiliari/A281305833_1.FAB","file/Andrano/UnitaImmobiliari/headerfilefabITA.csv","file/Andrano/UnitaImmobiliari/mappingFabbricatiUIITA.json");
+//    	importgr.importaUnitaImmobiliariITA2("file/Andrano/UnitaImmobiliari/A281305833_1.FAB","file/Andrano/UnitaImmobiliari/headerfilefabITA.csv","file/Andrano/UnitaImmobiliari/mappingFabbricatiUIITA.json");
+//		importgr.importaUnitaImmobiliariITA3("file/Andrano/UnitaImmobiliari/A281305833_1.FAB","file/Andrano/UnitaImmobiliari/headerfilefabITA.csv","file/Andrano/UnitaImmobiliari/mappingFabbricatiUIITA.json");
+//      importgr.importaSoggettiFabbricatiITA("file/Andrano/UnitaImmobiliariSoggetti/A281305833_1.SOG","file/Andrano/UnitaImmobiliariSoggetti/headerfilesogITA.csv","file/Andrano/UnitaImmobiliariSoggetti/mappingSoggettiFabbricatiITA.json");
+//		importgr.importaTitolaritaFabbricatiITA("file/Andrano/UnitaImmobiliariTitolarita/A281305833_1.TIT","file/Andrano/UnitaImmobiliariTitolarita/headerfiletitITA.csv","file/Andrano/UnitaImmobiliariTitolarita/mappingTitolaritaFabITA.json"); 
+//		importgr.importaParticelleFondiarieITA( "file/Andrano/ParticelleFondiarie/A281305835_1.TER","file/Andrano/ParticelleFondiarie/headerfileparfonITA.csv","file/Andrano/ParticelleFondiarie/mappingParticellaFondiariaITA.json");				
+//	    importgr.importaSoggettiFabbricatiITA("file/Andrano/ParticelleFondiarieSoggetti/A281305835_1.SOG","file/Andrano/ParticelleFondiarieSoggetti/headerfilesogfonITA.csv","file/Andrano/ParticelleFondiarieSoggetti/mappingSoggettiFondiariaITA.json");
+//		importgr.importaTitolaritaFabbricatiITA("file/Andrano/ParticelleFondiarieTitolarita/A281305835_1.TIT","file/Andrano/ParticelleFondiarieTitolarita/headerfiletitfonITA.csv","file/Andrano/ParticelleFondiarieTitolarita/mappingTitolaritaFondiariaITA.json"); 
+		
+
+		
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////    VOLANO   /////////////////////////////////////////////////////////////
+		
+		
+		
+		
+		//********************* FABBRICATI *************************************//		
 		System.out.println("1-Inserimento UnitaImmobiliari e Note. 4min"+date);
-	   //importgr.importaUnitaImmobiliari1("file/TN_file/IDR0000115470_TIPOFACSN_CAMML322.FAB","file/TN_header/headerfilefab.csv","file/file_mapping/mappingFabbricatiUI.json");
+	   importgr.importaUnitaImmobiliari1("file/Volano/UnitaImmobiliari/IDR0000115480_TIPOFACSN_CAMMM113.FAB","file/Volano/UnitaImmobiliari/headerfilefab.csv","file/Volano/UnitaImmobiliari/mappingFabbricatiUI.json");
 		
 		
 		// file di import, file header, file di mappings
         date = new Date();
         System.out.println("2-Inserimento Identificativi Catastali..6min"+date);
-//        importgr.importaUnitaImmobiliari2("file/TN_file/IDR0000115470_TIPOFACSN_CAMML322.FAB","file/TN_header/headerfilefab.csv","file/file_mapping/mappingFabbricatiUI.json");
+        
+        
+//        importgr.importaUnitaImmobiliari2("file/Volano/UnitaImmobiliari/IDR0000115480_TIPOFACSN_CAMMM113.FAB","file/Volano/UnitaImmobiliari/headerfilefab.csv","file/Volano/UnitaImmobiliari/mappingFabbricatiUI.json");
+
+      
+		date = new Date();
+		System.out.println("3-Inserimento UI..Indirizzi 3min"+date);
+//		importgr.importaUnitaImmobiliari3("file/Volano/UnitaImmobiliari/IDR0000115480_TIPOFACSN_CAMMM113.FAB","file/Volano/UnitaImmobiliari/headerfilefab.csv","file/Volano/UnitaImmobiliari/mappingFabbricatiUI.json");
+  
+		
+		date = new Date();
+		System.out.println("4-Inserimento Fabbricati..Soggetti "+dateFormat.format(date));
+//	  	importgr.importaSoggettiFabbricati("file/Volano/UnitaImmobiliariSoggetti/IDR0000115480_TIPOFACSN_CAMMM113.SOG","file/Volano/UnitaImmobiliariSoggetti/headerfilesog.csv","file/Volano/UnitaImmobiliariSoggetti/mappingSoggettiFabbricati.json");
+		
+		
+		date = new Date();
+		System.out.println("5-Inserimento Fabbricati..Titolarita 13min"+date);
+//		importgr.importaTitolaritaFabbricati("file/Volano/UnitaImmobiliariTitolarita/IDR0000115480_TIPOFACSN_CAMMM113.TIT","file/Volano/UnitaImmobiliariTitolarita/headerfiletit.csv","file/Volano/UnitaImmobiliariTitolarita/mappingTitolaritaFab.json");
+
+		
+//****************** Anagrafica ******************************************//
+		date = new Date();
+		System.out.println("6-Inserimento Anagrafica.. 5min"+date);
+//		importgr.importaAnagraficaComunale("file/Volano/AnagraficaComunale/anagrafe-DGASBANN.csv","file/Volano/AnagraficaComunale/mappingAnagraficaComunale.json","442");
+
+		date = new Date();
+	    System.out.println("7-Inserimento Anagrafica..famiglie "+date);
+//	    importgr.importaAnagraficaFamiglie("file/Volano/AnagraficaFamiglie/anagrafe-DGASBAN2.csv","file/Volano/AnagraficaFamiglie/mappingFamiglia.json","442");
+	
+	   
+//******************************* Particelle Fondiarie *********************************//	    
+	    
+		System.out.println("8-Inserimento Particelle fondiarie 20min"+date);
+//        importgr.importaParticelleFondiarie( "file/Volano/ParticelleFondiarie/442_41055.PAR","file/Volano/ParticelleFondiarie/headerfileparfon.csv","file/Volano/ParticelleFondiarie/mappingParticellaFondiaria.json","M113");
+	
+		date = new Date();
+		System.out.println("9-Inserimento Soggetti Particelle fondiarie "+date);
+//        importgr.importaSoggettiParticelleFondiarie("file/Volano/ParticelleFondiarieSoggetti/442_41055.SOG","file/Volano/ParticelleFondiarieSoggetti/headerfilesogfon.csv","file/Volano/ParticelleFondiarieSoggetti/mappingSoggettoFon.json");
+
+        
+        date = new Date();
+	    System.out.println("10-Inserimento Titolarita Particelle fondiarie 2 ore!!"+date);
+//        importgr.importaTitolaritaParticelleFondiarie("file/Volano/ParticelleFondiarieTitolarita/442_41055.TTC","file/Volano/ParticelleFondiarieTitolarita/headerfilettcfon.csv","file/Volano/ParticelleFondiarieTitolarita/mappingTitolaritaFondiaria.json");
+		
+	    
+	    
+	    
+	    
+//************* Contratti **************************************/
+		date = new Date();
+     	System.out.println("11-Inserimento Fornitura Energia "+date);
+//     	importgr.importaFornitureElettriche("file/Volano/ContrattiEnergia/VOLANO_Fornitura_Energia_dettaglio.xls","file/Volano/ContrattiEnergia/mappingFornituraEnergia.json");
+		
+     	date = new Date();
+	    System.out.println("12-Inserimento Fornitura Gas "+date);	
+//	    importgr.importaFornitureGas("file/Volano/ContrattiGas/VOLANO_Fornitura_Gas_dettaglio.xls","file/Volano/ContrattiGas/mappingFornituraGas.json");
+	    
+		date = new Date();
+		System.out.println("13-Inserimento Fornitura Locazione "+date);
+ //     importgr.importaContrattiLocazione("file/Volano/ContrattiLocazioni/VOLANO_Fornitura_Locazione2015_dettaglio.xls","file/Volano/ContrattiLocazioni/mappingFornituraLocazione.json");
+
+    
+    
+//*********************************  Utenze ********************************//
+		date = new Date();
+  	    System.out.println("14-Inserimento ICI AP "+date);
+//  	    importgr.importaTributiUtenzaICI_IMU_AbitazionePrincipale("file/Volano/TributiICI_IMU_AbitazionePrincipale/VOLANO_Utenze_I_C_I__I_M_U 2016 ABITAZIONE PRINCPALE_.xls","file/Volano/TributiICI_IMU_AbitazionePrincipale/mappingICIIMU.json");
+
+  	    
+  	    date = new Date();
+		System.out.println("15-Inserimento ICI NP "+date);
+
+//		importgr.importaTributiUtenzaICI_IMU_NudaProprieta("file/Volano/TributiICI_IMU_NudaProprieta/VOLANO_Utenze_I_C_I__I_M_U 2016 SOLO NUDI PROPRIETARI_.xls","file/Volano/TributiICI_IMU_NudaProprieta/mappingICIIMU.json");
+		date = new Date();
+		System.out.println("16-Inserimento Acqua "+date);
+
+		//	    importgr.importaTributiUtenzaAcqua("file/TN_file/TambilenoMaggio2018/utenze_attive_nel_2017_trambileno_da_nostro_gestionale.xls","file/file_mapping/mappingUtenzaAcqua.json");
+	    date = new Date();
+		System.out.println("17-Inserimento Lettura Acqua "+date);		
+
+//	    importgr.importaTributiLetturaAcqua("file/TN_file/TRAMBILENO_H2OExportlLETTURE_ixu3oqzmsnlv2bwhra5xvd3p1860500686.csv","file/file_mapping/mappingLetturaAcqua.json");
+	    date = new Date();
+		System.out.println("18-Inserimento Rifiuti "+date);
+
+//		importgr.importaTributiUtenzeRifiuti("file/Volano/UtenzeRifiuti/VOLANO_UtenzeRIFIUTI 2016.xls","file/Volano/UtenzeRifiuti/mappingUtenzaRifiuti.json");
+		date = new Date();
+		
+		
+		
+		date = new Date();
+		System.out.println("FINE"+date);
+	
+	
+
+		
+		
+		
+		
+		
+		
+		
+		/////////////////////////////////////////////////////////////////////////////////////////////
+		///////////////////////////////////////////////////////////////////////////////////////
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		//********************* FABBRICATI *************************************//		
+		System.out.println("1-Inserimento UnitaImmobiliari e Note. 4min"+date);
+//	   importgr.importaUnitaImmobiliari1("file/TN_file/IDR0000115470_TIPOFACSN_CAMML323.FAB_copy","file/TN_header/headerfilefab.csv","file/file_mapping/mappingFabbricatiUI.json");
+		
+		
+		// file di import, file header, file di mappings
+        date = new Date();
+        System.out.println("2-Inserimento Identificativi Catastali..6min"+date);
+      // importgr.importaUnitaImmobiliari2("file/TN_file/IDR0000115470_TIPOFACSN_CAMML322.FAB","file/TN_header/headerfilefab.csv","file/file_mapping/mappingFabbricatiUI.json");
 
       
 		date = new Date();
@@ -287,7 +455,7 @@ public static void main(String[] args) {
 //*********************************  Utenze ********************************//
 		date = new Date();
   	    System.out.println("14-Inserimento ICI AP "+date);
-//  	importgr.importaTributiUtenzaICI_IMU_AbitazionePrincipale("file/TN_file/TambilenoMaggio2018/TRAMBILENO_Utenze_I_C_I__I_M_U_ABPRINCIPALE2017.xls","file/file_mapping/mappingICIIMU.json");
+  //	importgr.importaTributiUtenzaICI_IMU_AbitazionePrincipale("file/TN_file/TambilenoMaggio2018/TRAMBILENO_Utenze_I_C_I__I_M_U_ABPRINCIPALE2017.xls","file/file_mapping/mappingICIIMU.json");
 
   	    
   	    date = new Date();
