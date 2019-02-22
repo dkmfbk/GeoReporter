cac1f0c8b6eda75666f1d5161b9975e699529fb2
gaetanocalabrese <calabrese@fbk.eu>
2019-02-22 12:55:33 +0100

    tolti degli import Set

--- GeoreporterWrappers/src/eu/fbk/dkm/georeporter/tn/wrappers/pojo/Titolarita.java
+++ GeoreporterWrappers/src/eu/fbk/dkm/georeporter/tn/wrappers/pojo/Titolarita.java
@@ -4,8 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
+
 
 public class Titolarita {
 
