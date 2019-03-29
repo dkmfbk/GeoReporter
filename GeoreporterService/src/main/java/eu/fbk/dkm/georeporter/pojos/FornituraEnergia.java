65c5ed60eac3aa8605c6f7edf7330dc066dcf999
gaetanocalabrese <calabrese@fbk.eu>
2019-03-29 14:02:08 +0100

    aggiunti metodi get set

--- GeoreporterService/src/main/java/eu/fbk/dkm/georeporter/pojos/FornituraEnergia.java
+++ GeoreporterService/src/main/java/eu/fbk/dkm/georeporter/pojos/FornituraEnergia.java
@@ -6,7 +6,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 //@XmlType(propOrder = { "id", "nome", "cognome", "eta", "telefono", "email"  })
 public class FornituraEnergia {
 
-	
+	String uri;
 	String denominazione;
 	String comune;
 	String codComune;
@@ -14,10 +14,82 @@ public class FornituraEnergia {
 	Integer idSiatelEnergiaDettaglio;
 	Integer ammontareFatturato;
 	Integer consumoFatturato;
-	String tipoUtenzaEnergia;
+	String  tipoUtenzaEnergia;
 	Integer kwFatturato;
 	Integer spesaConsumo;
 	Integer idSiatelEnergia;
+	public String getUri() {
+		return uri;
+	}
+	public void setUri(String uri) {
+		this.uri = uri;
+	}
+	public String getDenominazione() {
+		return denominazione;
+	}
+	public void setDenominazione(String denominazione) {
+		this.denominazione = denominazione;
+	}
+	public String getComune() {
+		return comune;
+	}
+	public void setComune(String comune) {
+		this.comune = comune;
+	}
+	public String getCodComune() {
+		return codComune;
+	}
+	public void setCodComune(String codComune) {
+		this.codComune = codComune;
+	}
+	public Integer getNumeroMesiFatturazione() {
+		return numeroMesiFatturazione;
+	}
+	public void setNumeroMesiFatturazione(Integer numeroMesiFatturazione) {
+		this.numeroMesiFatturazione = numeroMesiFatturazione;
+	}
+	public Integer getIdSiatelEnergiaDettaglio() {
+		return idSiatelEnergiaDettaglio;
+	}
+	public void setIdSiatelEnergiaDettaglio(Integer idSiatelEnergiaDettaglio) {
+		this.idSiatelEnergiaDettaglio = idSiatelEnergiaDettaglio;
+	}
+	public Integer getAmmontareFatturato() {
+		return ammontareFatturato;
+	}
+	public void setAmmontareFatturato(Integer ammontareFatturato) {
+		this.ammontareFatturato = ammontareFatturato;
+	}
+	public Integer getConsumoFatturato() {
+		return consumoFatturato;
+	}
+	public void setConsumoFatturato(Integer consumoFatturato) {
+		this.consumoFatturato = consumoFatturato;
+	}
+	public String getTipoUtenzaEnergia() {
+		return tipoUtenzaEnergia;
+	}
+	public void setTipoUtenzaEnergia(String tipoUtenzaEnergia) {
+		this.tipoUtenzaEnergia = tipoUtenzaEnergia;
+	}
+	public Integer getKwFatturato() {
+		return kwFatturato;
+	}
+	public void setKwFatturato(Integer kwFatturato) {
+		this.kwFatturato = kwFatturato;
+	}
+	public Integer getSpesaConsumo() {
+		return spesaConsumo;
+	}
+	public void setSpesaConsumo(Integer spesaConsumo) {
+		this.spesaConsumo = spesaConsumo;
+	}
+	public Integer getIdSiatelEnergia() {
+		return idSiatelEnergia;
+	}
+	public void setIdSiatelEnergia(Integer idSiatelEnergia) {
+		this.idSiatelEnergia = idSiatelEnergia;
+	}
 	
 	
 }
--- GeoreporterService/src/main/java/eu/fbk/dkm/georeporter/pojos/FornituraGas.java
+++ GeoreporterService/src/main/java/eu/fbk/dkm/georeporter/pojos/FornituraGas.java
@@ -16,6 +16,60 @@ public class FornituraGas {
 	Integer ConsumoFatturato;
 	String TipoUtenzaGas;
 	Integer IdSiatelGas;
+	public String getDenominazione() {
+		return Denominazione;
+	}
+	public void setDenominazione(String denominazione) {
+		Denominazione = denominazione;
+	}
+	public String getComune() {
+		return Comune;
+	}
+	public void setComune(String comune) {
+		Comune = comune;
+	}
+	public String getCodComune() {
+		return CodComune;
+	}
+	public void setCodComune(String codComune) {
+		CodComune = codComune;
+	}
+	public Integer getNumeroMesiFatturazione() {
+		return NumeroMesiFatturazione;
+	}
+	public void setNumeroMesiFatturazione(Integer numeroMesiFatturazione) {
+		NumeroMesiFatturazione = numeroMesiFatturazione;
+	}
+	public Integer getIdSiatelGasDettaglio() {
+		return IdSiatelGasDettaglio;
+	}
+	public void setIdSiatelGasDettaglio(Integer idSiatelGasDettaglio) {
+		IdSiatelGasDettaglio = idSiatelGasDettaglio;
+	}
+	public Integer getAmmontareFatturato() {
+		return AmmontareFatturato;
+	}
+	public void setAmmontareFatturato(Integer ammontareFatturato) {
+		AmmontareFatturato = ammontareFatturato;
+	}
+	public Integer getConsumoFatturato() {
+		return ConsumoFatturato;
+	}
+	public void setConsumoFatturato(Integer consumoFatturato) {
+		ConsumoFatturato = consumoFatturato;
+	}
+	public String getTipoUtenzaGas() {
+		return TipoUtenzaGas;
+	}
+	public void setTipoUtenzaGas(String tipoUtenzaGas) {
+		TipoUtenzaGas = tipoUtenzaGas;
+	}
+	public Integer getIdSiatelGas() {
+		return IdSiatelGas;
+	}
+	public void setIdSiatelGas(Integer idSiatelGas) {
+		IdSiatelGas = idSiatelGas;
+	}
 	
 	
 }
